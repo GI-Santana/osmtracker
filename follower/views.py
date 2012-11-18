3dec4efa94e1a4d1da9399c2a09297170c71c9aa
Steve Singer <steve@ssinger.info>
2012-11-18 18:20:09 -0500

    add in throttling

--- follower/views.py
+++ follower/views.py
@@ -2,7 +2,7 @@
 from django.http import HttpResponse
 from follower.models import Mapper
 from django.template import Context,loader
-import datetime,pytz
+import datetime,pytz,time
 
 def list(request):
     """
@@ -11,12 +11,15 @@ def list(request):
     in the last 24h we scan it for updates
     """
     tracker_list=Mapper.objects.all().order_by('-edit_date')
-    yesterday=datetime.datetime.now()-datetime.timedelta(days=1)
+    yesterday=datetime.datetime.now()-datetime.timedelta(days=1)    
     for mapper in tracker_list:
         if mapper.scan_date==None or  \
         mapper.scan_date.utctimetuple() < yesterday.utctimetuple():
             mapper.check_edits()
             mapper.save()
+            #sleep for 10 seconds after checking the
+            #RSS feed. We don't want to overload OSM
+            time.sleep(3)
     t=loader.get_template('follower/list.html')
     c=Context({'tracker_list':tracker_list})
     
