6dc7561790a280c0f88876b270da32e40da0e5d9
Steve Singer <steve@ssinger.info>
2012-11-18 20:06:32 -0500

    remember the first edit (in the rss feed)

--- follower/models.py
+++ follower/models.py
@@ -1,4 +1,5 @@
-import datetime,pytz
+from datetime import datetime
+import pytz
 from django.db import models
 import feedparser
 feedparser.USER_AGENT='OSMFollower/1.0 +http://mapexplorer.org'
@@ -8,11 +9,33 @@ class Mapper(models.Model):
     user=models.CharField(max_length=20)
     scan_date=models.DateTimeField('last_scan_date',null=True,blank=True)
     edit_date=models.DateTimeField('last_edit_date',null=True,blank=True)
-    
+    first_edit_date=models.DateTimeField('last_edit_date',null=True,blank=True)
     def check_edits(self):
         feed=feedparser.parse('http://www.openstreetmap.org/user/'
                           + self.user + '/edits/feed')
         if len(feed.entries) > 0:
-            self.edit_date=feed.entries[0].published
-            self.scan_date=datetime.datetime.now(pytz.utc)
+            published_parsed=feed.entries[0].published_parsed
+            self.edit_date=datetime(published_parsed.tm_year
+                                    ,published_parsed.tm_mon
+                                    ,published_parsed.tm_mday
+                                    ,published_parsed.tm_hour
+                                    ,published_parsed.tm_min
+                                    ,published_parsed.tm_sec
+                                    ,0
+                                    ,pytz.utc)
+            published_parsed=feed.entries[-1].published_parsed
+            first_edit_date=datetime(published_parsed.tm_year
+                                     ,published_parsed.tm_mon
+                                     ,published_parsed.tm_mday
+                                     ,published_parsed.tm_hour
+                                     ,published_parsed.tm_min
+                                     ,published_parsed.tm_sec
+                                     ,0
+                                     ,pytz.utc)
+
+            is_older_edit=first_edit_date < self.first_edit_date
+            if self.first_edit_date==None or is_older_edit:
+                self.first_edit_date=first_edit_date
+                
+            self.scan_date=datetime.now(pytz.utc)
 
