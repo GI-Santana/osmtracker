1e50ae3f8849efca96cf25f80c483aa91bdc355d
Steve Singer <steve@ssinger.info>
2012-12-30 22:58:04 -0500

    whitespace changes

--- follower/models.py
+++ follower/models.py
@@ -1,15 +1,9 @@
 from django.db import models
-#from follower.reachout import ReachOut
 
 # Create your models here.
 
 class Email(models.Model):
     text=models.TextField()
     subject=models.CharField(max_length=50)
-  
-  #def is_sent(self):
-  #      usages=ReachOut.objects.all().filter(email=self.id)
-  #      return len(usages)>0
-    
 
-    
+
